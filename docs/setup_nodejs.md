74f1fa4dc62279985ac451a8167c519497b897ba
Frederik Goovaerts <info@frederik.dev>
2020-08-29 16:21:35 +0200

    Make scaffold changes to simplify presented frontend

---
+++ docs/setup_nodejs.md
@@ -0,0 +1,7 @@
+---
+id: setup_nodejs
+title: Setting up a Node.js project from scratch
+sidebar_label: Project Setup - Node.js
+---
+
+This is content
--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    Practical: ["setup_nodejs"],
+    Old: ["doc1", "doc2", "doc3", "mdx"],
   },
 };
